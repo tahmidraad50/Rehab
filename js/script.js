6562ac0be4bc228ef3378d5e9cd5562b069ec41b
tahmidraad50 <2014755018@uits.edu.bd>
2024-06-28 17:34:45 +0600

    2nd commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+$('.slider-part').slick({
+  dots: false,
+  infinite: true,
+  speed: 250,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+    prevArrow:false,
+    autoplay:true,
+    nextArrow:false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+		
+$('.testimoni-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 250,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+    prevArrow:false,
+    autoplay:true,
+    nextArrow:false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+		
